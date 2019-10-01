30a47ba7664f9cc271ad19bd17423022374d91df
juan <mtzn_juandaniel@yahoo.com.mx>
2019-10-01 10:04:00 -0600

    Botones cobrar y cancelar

--- views/home.view.php
+++ views/home.view.php
@@ -10,23 +10,33 @@ for($i = 1; $i < $countLength + 1; $i++) {
     <?php foreach($orders as $order): ?>
 
     <ul>
-        <li><?php print_r($order['name_food'].' '.$order['name_ing']); ?></li>
+        <li class="foodList"><?php print_r($order['name_food'].' '.$order['name_ing']); ?></li>
     </ul>
 
     <?php
     $total += $order['price'];
     endforeach;
     ?>
-    
-    <h2 class="total"><?php echo $total; ?></h2>
+
+    <h2 class="total"><?php echo '$ '.$total; ?></h2>
+
+    <div class="sell-buttons">
+        <div class="chek" id="chek">
+            <i class="fas fa-dollar-sign"></i>
+        </div>
+
+        <div class="cancel" id="cancel">
+            <i class="fas fa-times"></i>
+        </div>
+    </div>
 </div>
 
 <?php } ?>
 
 <div class="report">
-    <button class="newOrder">
+    <div class="newOrder" id="newOrder">
         <i class="far fa-plus-square"></i>
-    </button>
+    </div>
 </div>
 
 <div class="setOrder" id="setOrder">
